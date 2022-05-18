2332b3c18217499bae40b6ecbcb95eda508397b9
neverenought <103130410+neverenought@users.noreply.github.com>
2022-05-18 16:48:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,23 +24,23 @@ class Game_sprite(sprite.Sprite):
 
 class Dino(Game_sprite):
     def __init__(self):
-        super().__init__(image.load('dino.jpg'), 100, 300, 100, 150)
+        super().__init__(image.load('dino.png'), 100, 300, 100, 150)
         self.isJump = False
-        self.jumpCount = -15
+        self.jumpCount = -20
 
     def update(self):
         if self.isJump:
             self.rect.y += self.jumpCount
             self.jumpCount += 1
-            if self.jumpCount > 15:
-                self.jumpCount = -15
+            if self.jumpCount > 20:
+                self.jumpCount = -20
                 self.isJump = False
 
         super().update()
 
 class Cactus(Game_sprite):
     def __init__(self):
-        super().__init__(image.load('cactus.png'),900,200,50,100)
+        super().__init__(image.load('cactus.png'),900,350,50,100)
 
     def update(self):
         self.rect.x -= 6
@@ -60,23 +60,41 @@ game=True
 menu = False
 finish = False
 clock = time.Clock()
+
 while game:
     clock.tick(60)
     for e in event.get():
         if e.type == QUIT:
             game = False
+        if e.type == MOUSEBUTTONDOWN:
+            if e.button == 1:
+                x,y = mouse.get_pos()
+                if menu:
+                    if x >350 and x< 550 and y>200 and y<280:
+                        menu = False
+                        finish = False
+                        
+                        dino = Dino()
+                        cactus = Cactus()
+
+                    if x > 350 and x <550 and y>400 and y<480:
+                        game = False
         if e.type == KEYDOWN:
             if e.key == K_SPACE:
                 dino.isJump = True
 
-    screen.blit(background, (0,0))
+
+    screen.blit(background,(0,0))
     if menu:
-        screen.blit(button_play,(360,200))
+        screen.blit(button_play,(350,200))
         screen.blit(button_quit,(350,400))
     elif not(finish):
+        if dino.rect.colliderect(cactus.rect):
+            finish = True
+            menu = True
+        #тут игровой процесс 
         dino.update()
         cactus.update()
 
 
     display.update()
-    
